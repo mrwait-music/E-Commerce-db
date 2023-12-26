d4035e619c387b228adc6963ab8e36067398be08
Mr Wait <brennanmitchell1192@gmail.com>
2023-12-26 12:23:51 -0600

    product-routes file written and tested.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,16 +4,52 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const data = await Product.findAll({
+      include: [
+      { model: Category, as: 'category' },
+      { model: Tag, as: 'tags' }]
+    });
+
+    if (!data) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  try {
+    const data = await Product.findByPk(req.params.id, {
+      include: [
+      { model: Category, as: 'category'},
+      { model: Tag, as: 'tags' }]
+    });
+    console.log(data);
+    if (!data) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+}
+  );
+
 
 // create new product
 router.post('/', (req, res) => {
